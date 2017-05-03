2dd8120dcf409a3cdef60ebd1fb49806260b40fa
Sowmya Dinavahi and Trevor Fast <trevor@>
2017-05-03 14:45:27 -0700

    add and pass all method to specs

--- lib/animal_shelter.rb
+++ lib/animal_shelter.rb
@@ -1,6 +1,6 @@
 class Animal
   attr_accessor(:name, :gender, :date_of_admittance, :type, :breed, :id)
-  @@animals = []
+
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
@@ -8,15 +8,31 @@ class Animal
     @date_of_admittance = attributes.fetch(:date_of_admittance)
     @type = attributes.fetch(:type)
     @breed = attributes.fetch(:breed)
+    @id = attributes.fetch(:id)
 
-  end
-
-  define_singleton_method(:all) do
-    @@animals
   end
 
   define_method(:save) do
-    @@animals.push(self)
+    result = DB.exec("INSERT INTO add_animal (name,gender,date_of_admittance,type,breed) VALUES('#{name}','#{gender}','#{date_of_admittance}','#{type}','#{breed}') RETURNING id;")
+    @id = result.first.fetch("id").to_i
   end
 
+  define_singleton_method (:all) do
+    returned_animals = DB.exec("SELECT * FROM add_animal;")
+    animals = []
+    returned_animals.each do |animal|
+      name = animal.fetch("name")
+      gender = animal.fetch("gender")
+      date = animal.fetch("date_of_admittance")
+      type = animal.fetch("type")
+      breed = animal.fetch("breed")
+      id = animal.fetch("id").to_i
+      animals.push(Animal.new({:name => name, :gender => gender, :date_of_admittance => date, :type => type, :breed => breed, :id => id}))
+    end
+    animals
+  end
+
+  define_method(:==) do |another_animal|
+    self.name().==(another_animal.name()).&(self.gender().==(another_animal.gender))&(self.date_of_admittance().==(another_animal.date_of_admittance())).&(self.type().==(another_animal.type())).&(self.breed().==(another_animal.breed()))
+  end
 end
--- spec/animal_shelter_spec.rb
+++ spec/animal_shelter_spec.rb
@@ -2,57 +2,90 @@ require "animal_shelter"
 require "rspec"
 require "pry"
 
+DB = PG.connect({:dbname => "animal_shelter"})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM add_animal *;")
+    DB.exec("DELETE FROM add_person *;")
+  end
+end
+
 describe (Animal) do
 
   describe '#initialize' do
     it("initializes the system to add animals") do
-      test_animal = Animal.new({:name => 'rufus', :gender => 'male', :date_of_admittance => '2017-06-04', :type => 'fish', :breed => 'carp'})
+      test_animal = Animal.new({:name => 'rufus', :gender => 'male', :date_of_admittance => '2017-06-04', :type => 'fish', :breed => 'carp',:id => nil})
       expect(test_animal.name()).to(eq("rufus"))
     end
   end
 
   describe '#initialize' do
     it('lets you see gender') do
-      test_animal = Animal.new({:name => 'rufus', :gender => 'male', :date_of_admittance => '2017-06-04', :type => 'fish', :breed => 'carp'})
+      test_animal = Animal.new({:name => 'rufus', :gender => 'male', :date_of_admittance => '2017-06-04', :type => 'fish', :breed => 'carp', :id => nil})
       expect(test_animal.gender()).to(eq("male"))
     end
   end
 
   describe '#initialize' do
     it('lets you check the date of admittance') do
-    test_animal = Animal.new({:name => 'rufus', :gender => 'male', :date_of_admittance => '2017-06-04', :type => 'fish', :breed => 'carp'})
+    test_animal = Animal.new({:name => 'rufus', :gender => 'male', :date_of_admittance => '2017-06-04', :type => 'fish', :breed => 'carp', :id => nil})
     expect(test_animal.date_of_admittance()).to(eq("2017-06-04"))
     end
   end
 
   describe '#initialize' do
     it('lets you check type of the animal') do
-    test_animal = Animal.new({:name => 'rufus', :gender => 'male', :date_of_admittance => '2017-06-04', :type => 'fish', :breed => 'carp'})
+    test_animal = Animal.new({:name => 'rufus', :gender => 'male', :date_of_admittance => '2017-06-04', :type => 'fish', :breed => 'carp', :id => nil})
     expect(test_animal.type()).to(eq('fish'))
     end
   end
 
   describe '#initialize' do
     it('lets you check the breed') do
-    test_animal = Animal.new({:name => 'rufus', :gender => 'male', :date_of_admittance => '2017-06-04', :type => 'fish', :breed => 'carp'})
+    test_animal = Animal.new({:name => 'rufus', :gender => 'male', :date_of_admittance => '2017-06-04', :type => 'fish', :breed => 'carp', :id => nil})
     expect(test_animal.breed()).to(eq('carp'))
     end
   end
 
     describe '.all' do
       it('empties the array initially') do
+        # test_animal = Animal.new({:name => 'rufus', :gender => 'male', :date_of_admittance => '2017-06-04', :type => 'fish', :breed => 'carp', :id => nil})
       expect(Animal.all()).to(eq([]))
     end
   end
 
     describe '#save' do
       it('it saves to the empty array') do
-        test_animal = Animal.new({:name => 'rufus', :gender => 'male', :date_of_admittance => '2017-06-04', :type => 'fish', :breed => 'carp'})
+        test_animal = Animal.new({:name => 'rufus', :gender => 'male', :date_of_admittance => '2017-06-04', :type => 'fish', :breed => 'carp', :id => nil})
         test_animal.save()
         expect(Animal.all()).to(eq([test_animal]))
-        test_animal2 = Animal.new({:name => 'murphy', :gender => 'female', :date_of_admittance => '2017-06-03', :type => 'bird', :breed => 'finch'})
+        test_animal2 = Animal.new({:name => 'murphy', :gender => 'female', :date_of_admittance => '2017-06-03', :type => 'bird', :breed => 'finch', :id => nil})
         test_animal2.save()
         expect(Animal.all()).to(eq([test_animal,test_animal2]))
       end
     end
-end
+
+    describe '#==' do
+      it 'is the same animal if it has the same id' do
+        test_animal = Animal.new({:name => 'rufus', :gender => 'male', :date_of_admittance => '2017-06-04', :type => 'fish', :breed => 'carp', :id => nil})
+        test_animal2 = Animal.new({:name => 'rufus', :gender => 'male', :date_of_admittance => '2017-06-04', :type => 'fish', :breed => 'carp', :id => nil})
+        expect(test_animal).to(eq(test_animal2))
+      end
+    end
+
+    describe("#id") do
+      it("lets you read the animal ID") do
+        animal = Animal.new({:name => 'rufus', :gender => 'male', :date_of_admittance => '2017-06-04', :type => 'fish', :breed => 'carp', :id => nil})
+        expect(animal.id()).to(eq(nil))
+      end
+    end
+
+    # describe("#find") do
+    #   it("allows shelter to find people to adopt an animal") do
+    #     patient = Animal.new({:name => 'rufus', :gender => 'male', :date_of_admittance => '2017-06-04', :type => 'fish', :breed => 'carp'})
+    #     expect(shelter.find).to(eq())
+    #   end
+    # end
+
+  end
